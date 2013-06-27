5dc5325cf51993f13f3c29fc50f0770ec8ec6960
Michael Kostenchuk <kmi9.other@gmail.com>
2013-06-27 12:54:25 +0400

    generate empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UaoWork::Application.config.secret_token = 'ec75f751b622320cb44c71a734a76bbbf651ea428528c8435f2f7faa661f66839796f0018153c76bb22d4c7d9c8a2943ae0ae71001b3095c089f6218ff1c557e'
